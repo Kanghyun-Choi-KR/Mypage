591cd820af764b731c20c150f87c8c0f37848ddc
Kanghyun Choi <ckh971008@gmail.com>
2026-02-16 23:04:50 +0900

    test

--- assets/js/albums.js
+++ assets/js/albums.js
@@ -1,26 +1,38 @@
 document.addEventListener("DOMContentLoaded", async () => {
 
   const albums = [
+    
     {
-      title: "Seung-yoon Lee, 역성",
+      title: "The Rise And Fall Of Ziggy Stardust And The Spiders From Mars",
+      artist: "David Bowie",
+      img: "album_art/davidbowie_stardust.jpg",
+      note: "album_note/davidbowie_stardust.html"
+    },
+
+    {
+      title: "역성",
+      artist: "Seung-yoon Lee",
       img: "album_art/leeseungyoon_yeokseong.jpg",
       note: "album_note/leeseungyoon_yeokseong.html"
     },
     
     {
-      title: "Seung-yoon Lee, 꿈의 거처",
+      title: "꿈의 거처",
+      artist: "Seung-yoon Lee",
       img: "album_art/leeseungyoon_dream.jpg",
       note: "album_note/leeseungyoon_dream.html"
     },
 
     {
-      title: "Anri, TIMELY!!",
+      title: "TIMELY!!",
+      artist: "Anri",
       img: "album_art/anri_timely.jpg",
       note: "album_note/anri_timely.html"
     },
     
     {
-      title: "Anri, Heaven Beach",
+      title: "Heaven Beach",
+      artist: "Anri",
       img: "album_art/anri_heavenbeach.jpg",
       note: "album_note/anri_heavenbeach.html"
     }
@@ -44,14 +56,19 @@ document.addEventListener("DOMContentLoaded", async () => {
     section.className = "banner";
 
     section.innerHTML = `
-            <div class="content">
-                <header><h2>${album.title}</h2></header>
-                        ${noteHtml}
-                              </div>
-                                    <span class="image object">
+      <div class="content">
+          <header>
+            <h2 class="album-title">${album.title}</h2>
+                  <div class="album-artist">${album.artist}</div>
+                      </header>
+                          <div class="album-desc">
+                                ${noteHtml}
+                                    </div>
+                                      </div>
+                                        <span class="image object">
                                             <img src="${album.img}" alt="">
-                                                  </span>
-                                                      `;
+                                              </span>
+                                              `;
 
     list.appendChild(section);
   }
