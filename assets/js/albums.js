c0d8b68f7cb0effe645af8f5455ff28be3e27e7e
Kanghyun Choi <ckh971008@gmail.com>
2026-02-17 22:56:40 +0900

    Hotfix

--- assets/js/albums.js
+++ assets/js/albums.js
@@ -53,31 +53,35 @@ document.addEventListener("DOMContentLoaded", async () => {
       noteHtml = "<p>Can't read the content</p>";
     }
 
+    const imageHtml = album.link
+      ? `<a href="${album.link}" target="_blank" rel="noopener">
+                 <img src="${album.img}" alt="">
+                        </a>`
+      : `<img src="${album.img}" alt="">`;
+
+
     const section = document.createElement("section");
     section.className = "banner";
 
     section.innerHTML = `
-      <div class="content">
-          <header>
-            <h2 class="album-title js-title" ${album.title}">
-              ${album.title}
-              </h2>
-                  <div class="album-artist">${album.artist}</div>
-                      </header>
-                          <div class="album-desc">
-                                ${noteHtml}
-                                    </div>
-                                      </div>
-                                        <span class="image object">
-                                          ${album.link ? `
-                                              <a href="${album.link}" target="_blank" rel="noopener">
-                                                    <img src="${album.img}" alt="${album.title}">
-                                                        </a>
-                                                          ` : `
-                                                                <img src="${album.img}" alt="${album.title}">
-                                                                  `}
-                                        </span>
-                                              `;
+        <div class="content">
+            <header>
+                    <h2 class="album-title js-title" data-title="${album.title}">
+                              ${album.title}
+                                      </h2>
+                                              <div class="album-artist">${album.artist}</div>
+                                                    </header>
+
+            <div class="album-desc">
+              ${noteHtml}
+                    </div>
+                        </div>
+
+          <span class="image object">
+            ${imageHtml}
+                </span>
+                  `;
+
 
     list.appendChild(section);
   }
